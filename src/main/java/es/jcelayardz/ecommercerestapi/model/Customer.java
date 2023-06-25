c9d3dd0fe96b87b1371f034ff4de436250d13350
JCelayaRdz <jcelayardz@gmail.com>
2023-06-25 22:30:22 +0200

    add customer model

---
+++ src/main/java/es/jcelayardz/ecommercerestapi/model/Customer.java
@@ -0,0 +1,87 @@
+package es.jcelayardz.ecommercerestapi.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @Column(nullable = false, unique = true, length = 25)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 70)
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;
+
+    private Date dateOfBirth;
+
+    public Customer() {
+    }
+
+    public Customer(String username, String email, String password, Gender gender, Date dateOfBirth) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer = {\n" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", gender=" + gender +
+                ", dateOfBirth=" + dateOfBirth +
+                "}\n";
+    }
+}
---
+++ src/main/java/es/jcelayardz/ecommercerestapi/model/Gender.java
@@ -0,0 +1,7 @@
+package es.jcelayardz.ecommercerestapi.model;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
