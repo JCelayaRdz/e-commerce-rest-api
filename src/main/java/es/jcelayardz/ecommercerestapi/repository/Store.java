35f93bd95d74b4f884f2660752e478c26387119c
JCelayaRdz <jcelayardz@gmail.com>
2023-06-26 21:42:57 +0200

    add store repository

---
+++ src/main/java/es/jcelayardz/ecommercerestapi/repository/Store.java
@@ -0,0 +1,6 @@
+package es.jcelayardz.ecommercerestapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Store extends JpaRepository<Store, String> {
+}
