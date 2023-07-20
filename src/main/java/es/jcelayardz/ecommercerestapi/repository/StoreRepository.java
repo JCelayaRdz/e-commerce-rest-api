14c3a89decbde16eca1b55352eb22c7f139ccdaf
JCelayaRdz <jcelayardz@gmail.com>
2023-07-20 23:47:17 +0200

    add method to find a store by name

--- src/main/java/es/jcelayardz/ecommercerestapi/repository/StoreRepository.java
+++ src/main/java/es/jcelayardz/ecommercerestapi/repository/StoreRepository.java
@@ -3,5 +3,8 @@ package es.jcelayardz.ecommercerestapi.repository;
 import es.jcelayardz.ecommercerestapi.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoreRepository extends JpaRepository<Store, String> {
+    Optional<Store> findByName(String name);
 }
