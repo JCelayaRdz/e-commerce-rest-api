6846c1156a6a94e9e11a66691461aab842681265
JCelayaRdz <jcelayardz@gmail.com>
2023-07-20 22:46:59 +0200

    add custom method to filter by visible products

--- src/main/java/es/jcelayardz/ecommercerestapi/repository/ProductRepository.java
+++ src/main/java/es/jcelayardz/ecommercerestapi/repository/ProductRepository.java
@@ -2,6 +2,12 @@ package es.jcelayardz.ecommercerestapi.repository;
 
 import es.jcelayardz.ecommercerestapi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query("SELECT p FROM Product p WHERE p.isVisible = true")
+    List<Product> findAllVisible();
 }
