6337e715dc292c778ecb8fd9af907de94bb1bf37
JCelayaRdz <jcelayardz@gmail.com>
2023-07-20 18:38:43 +0200

    fix repository model type

--- src/main/java/es/jcelayardz/ecommercerestapi/repository/StoreRepository.java
+++ src/main/java/es/jcelayardz/ecommercerestapi/repository/StoreRepository.java
@@ -1,6 +1,7 @@
 package es.jcelayardz.ecommercerestapi.repository;
 
+import es.jcelayardz.ecommercerestapi.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreRepository extends JpaRepository<StoreRepository, String> {
+public interface StoreRepository extends JpaRepository<Store, String> {
 }
