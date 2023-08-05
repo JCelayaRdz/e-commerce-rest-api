ac7b45c6a1e3cd7c74584b7e1a26a1e2dc9d4706
JCelayaRdz <jcelayardz@gmail.com>
2023-08-05 20:55:20 +0200

    update method to be able to hide products

--- src/main/java/es/jcelayardz/ecommercerestapi/service/ProductService.java
+++ src/main/java/es/jcelayardz/ecommercerestapi/service/ProductService.java
@@ -49,7 +49,6 @@ public class ProductService {
     }
 
     public ProductDto updateProduct(Integer productId, ProductDto productDto) {
-        // TODO: add field "isVisible" to product dto so it can be hidden
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new ProductNotFoundException(productId));
 
@@ -58,7 +57,9 @@ public class ProductService {
         if (productDto.getDescription() != null) {
             product.setDescription(productDto.getDescription());
         }
-        // set here the isVisible field
+        if (productDto.isVisible() != null) {
+            product.setVisible(productDto.isVisible());
+        }
 
         return productRepository.save(product).toDto();
     }
