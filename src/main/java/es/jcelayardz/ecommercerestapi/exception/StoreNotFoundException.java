1d80f9be3d1356f63f012a053494d63cd0f76e05
JCelayaRdz <jcelayardz@gmail.com>
2023-08-29 10:25:47 +0200

    fix grammatical error in exception message

--- src/main/java/es/jcelayardz/ecommercerestapi/exception/StoreNotFoundException.java
+++ src/main/java/es/jcelayardz/ecommercerestapi/exception/StoreNotFoundException.java
@@ -2,6 +2,6 @@ package es.jcelayardz.ecommercerestapi.exception;
 
 public class StoreNotFoundException extends RuntimeException {
     public StoreNotFoundException(String storeName) {
-        super("Could not found store with name " + storeName);
+        super("Could not find store with name " + storeName);
     }
 }
