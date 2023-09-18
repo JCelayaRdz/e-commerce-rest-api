cc992811cd21bdab0da09edd70323010df950709
JCelayaRdz <jcelayardz@gmail.com>
2023-09-18 21:27:51 +0200

    create admindto class

---
+++ src/main/java/es/jcelayardz/ecommercerestapi/dto/AdminDto.java
@@ -0,0 +1,73 @@
+package es.jcelayardz.ecommercerestapi.dto;
+
+import es.jcelayardz.ecommercerestapi.model.AdminType;
+
+public class AdminDto {
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private AdminType adminType;
+
+    private StoreDto store;
+
+    public AdminDto(String username, String email, String password, AdminType adminType) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.adminType = adminType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AdminType getAdminType() {
+        return adminType;
+    }
+
+    public void setAdminType(AdminType adminType) {
+        this.adminType = adminType;
+    }
+
+    public StoreDto getStore() {
+        return store;
+    }
+
+    public void setStore(StoreDto store) {
+        this.store = store;
+    }
+
+    @Override
+    public String toString() {
+        return "AdminDto = {\n" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", adminType=" + adminType +
+                "\n";
+    }
+}
